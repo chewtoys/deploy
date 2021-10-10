b4133970d78ea2341d3570ac349b6b23bb9a3eb1
staging <staging@pastvu.com>
2021-10-10 11:52:53 -0400

    remove bin directory

--- bin/create-sshfs-volume.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-. ~/env/production-server.env
-
-docker volume create --driver vieux/sshfs \
-  -o sshcmd=$USER@$IP:$STORE_PATH \
-  -o ro \
-  -o reconnect \
-  -o allow_other \
-  pastvu_store
--- bin/download-real-dump.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. ~/env/production-server.env
-P=$USER@$IP
-FILE=$(ssh $P find $MONGO_BACKUPS_PATH|sort|tail -1)
-mkdir -p ~/dumps
-scp $P:$FILE ~/dumps
