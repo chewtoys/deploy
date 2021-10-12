128b21fe294710c0a85c501a50594b80bd649868
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2021-10-13 01:59:08 +0300

    cleanup deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env -S -i bash
 set -exu
 shopt -s expand_aliases
-source ./share/eslint.subr
-source ./share/aliases
+alias mktag="shasum|cut -c1-16"
 source ~/env/pastvu.env
 
 # Set versions
@@ -28,9 +27,6 @@ export DOMAIN
 export CONFIG=./config/${PASTVU_ENV}.js
 export CONFIG_TAG=$(cat ${CONFIG}|mktag)
 export DEPLOY_TAG=$(date|mktag)
-
-# Lint config
-# eslint ${CONFIG}
 
 # Do the job
 docker node update --label-add pastvu.pastvu-data=true ${NODE_ID}
