fd29799b9195fc30d5682ba7619d1c59c7130721
staging <staging@pastvu.com>
2021-10-17 22:32:52 -0400

    1.4.7

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ alias mktag="shasum|cut -c1-16"
 source ~/env/pastvu.env
 
 # Set versions
-export TAG="1.4.6"
-export TAG_EN="1.4.6-en"
+export TAG="1.4.7"
+export TAG_EN="1.4.7-en"
 export TAG_FILESERVER="1.0.3"
 
 case ${PASTVU_ENV} in
