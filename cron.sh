3997c140b61dc3e1e5bacc8ae9b243d83f7f4dff
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2022-12-26 04:37:56 +0300

    add cron CD

---
+++ cron.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. /etc/telelog.subr
+
+cd $(dirname $0)
+
+# https://stackoverflow.com/questions/3258243/check-if-pull-needed-in-git
+git remote update
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+	echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+	echo "Need to pull"
+	git pull && \
+	./deploy.sh && \
+	telelog "Application updated" || telelog "Failed to update application"
+elif [ $REMOTE = $BASE ]; then
+	echo "Need to push"
+else
+	echo "Diverged"
+fi
